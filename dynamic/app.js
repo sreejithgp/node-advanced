2c62c121b0bf69cec0d7db93d50d8cb1d10ba485
Sreejith Pillai <sreejith.gp@icloud.com>
2018-12-08 17:22:15 +0530

    sharing data with users

--- dynamic/app.js
+++ dynamic/app.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminDate = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminDate.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000);
+app.listen(4000);
