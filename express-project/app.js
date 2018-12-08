d59debf4793291f687eb88cbe5b32c377b3c7b6b
Sreejith Pillai <sreejith.gp@icloud.com>
2018-12-08 13:21:14 +0530

    parsing request body

--- express-project/app.js
+++ express-project/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+
 app.use('/add_product', (req, res, next) => {
-  res.send('<h1>Add product</h>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
