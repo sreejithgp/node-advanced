0edffeead33b7b475423cf831460945edd7ebc5f
Sreejith Pillai <sreejith.gp@icloud.com>
2019-01-05 11:30:07 +0530

    saving products to file

--- mvc/models/product.js
+++ mvc/models/product.js
@@ -1,4 +1,20 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'producs.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if(err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+}
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +22,13 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (e) => console.log(e));
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 }
